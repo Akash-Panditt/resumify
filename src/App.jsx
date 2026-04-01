f1e690feaed4c8dac68124244cba77f36e067909
skyakashpandit <akashpandit8436@gmail.com>
2026-04-01 14:57:55 +0530

    basic setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react"; 
+
+const App = () => {
+  return (
+    <div>
+      <h1>Resumify</h1>
+      <p>Welcome to Resumify, your one-stop solution for creating stunning resumes effortlessly!</p>
+    </div>
+  );
+};
